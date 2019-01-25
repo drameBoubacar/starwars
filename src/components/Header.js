ee5d9b2bef18b9f2b61c8e0efbfefec92c8cd5aa
drameBoubacar <boubacardrame9@gmail.com>
2019-01-25 14:27:25 +0000

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+  return(
+    <div>
+      <section className="header">
+        
+      </section>
+    </div>
+  )
+}
+
+export default Header;
