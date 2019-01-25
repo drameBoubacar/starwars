40fcfc796e6b3fafc52b71a625bc2e19655094f9
drameBoubacar <boubacardrame9@gmail.com>
2019-01-25 17:52:06 +0000

    added function map and navbar fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,29 @@
 import React,{Component} from 'react';
 
-// const NavBar = () => {
-//   return(
-//     <div>
-//       <nav>
-//         <ul className="nav-ul">
-//           <li>le coté lumineux</li>
-//           <li>le coté obscur</li>
-//           <li>l'empire</li>
-//           <li>les droïdes</li>
-//           <li>les ewoks</li>
-//           <li>jabba le huit</li>
-//         </ul>
-//       </nav>
-//     </div>
-//   )
-// }
+const colorLink = {
+  color : 'blue'
+}
 
 class NavBar extends Component {
   constructor(){
     super();
     this.state = {
-      link: ['le coté lumineux', 'le coté obscur', 'l\'empire', 'les droïdes', 'les ewoks', 'jabba le huit']
+      link: ['le coté lumineux', 'le coté obscur', 'l\'empire', 'les droïdes', 'les ewoks', 'jabba le huit'],
+      hover : colorLink
     }
   }
+  handleHover = () => {
+    this.setState({
+      hover : colorLink
+    })
+  }
   render(){
+
     return(
       <div>
         <nav>
           <ul className="nav-ul">
-            <li>{this.state.link[0]}</li>
-            <li>{this.state.link[1]}</li>
-            <li>{this.state.link[2]}</li>
-            <li>{this.state.link[3]}</li>
-            <li>{this.state.link[4]}</li>
-            <li>{this.state.link[5]}</li>
+          {this.state.link.map(function (link, index){ return  <li key={index}>{link}</li>}) }
           </ul>
         </nav>
         
