3694f93d0da65a5f9ad8c39a28a947bc55b368a0
drameBoubacar <boubacardrame9@gmail.com>
2019-01-25 14:16:37 +0000

    added component navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavBar = () => {
+  return(
+    <div>
+      <nav>
+        <ul className="list-nav">
+          <li>le coté lumineux</li>
+          <li>le coté obscur</li>
+          <li>l'empire</li>
+          <li>les droïdes</li>
+          <li>les ewoks</li>
+          <li>jabba le huit</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar;
