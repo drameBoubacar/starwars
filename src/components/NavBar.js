9b5257ecf42b3fbae7f3ea152c631f7dd76ec0f4
drameBoubacar <boubacardrame9@gmail.com>
2019-01-25 18:17:38 +0000

    add function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,22 @@
 import React,{Component} from 'react';
 
-const colorLink = {
-  color : 'blue'
-}
-
 class NavBar extends Component {
   constructor(){
     super();
     this.state = {
-      link: ['le coté lumineux', 'le coté obscur', 'l\'empire', 'les droïdes', 'les ewoks', 'jabba le huit'],
-      hover : colorLink
+      link: ['le coté lumineux', 'le coté obscur', 'l\'empire', 'les droïdes', 'les ewoks', 'jabba le huit']
     }
   }
-  handleHover = () => {
-    this.setState({
-      hover : colorLink
-    })
-  }
   render(){
-
+    
     return(
       <div>
         <nav>
           <ul className="nav-ul">
-          {this.state.link.map(function (link, index){ return  <li key={index}>{link}</li>}) }
+          {this.state.link.map(
+            (link, index) => 
+              <li key={index}>{link}</li>
+          )}
           </ul>
         </nav>
         
