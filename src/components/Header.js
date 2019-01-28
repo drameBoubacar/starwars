ce148fe0930f25487b53a5bfdf7ee8de840c9cd3
drameBoubacar <boubacardrame9@gmail.com>
2019-01-28 17:12:01 +0000

    error resolved

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,14 @@
-import React,{Component} from 'react';
+import React from 'react';
 
 
-// const Header = () => {
-//   return(
-//     <div>
-//       <section className="header">
+const Header = () => {
+  return(
+    <div>
+      <section className="header">
         
-//       </section>
-//     </div>
-//   )
-// }
-
-// export default Header;
-
-export default class Header extends Component {
-  handleScroll = () => {
-    console.log('scrolling');
-  }
-  render(){
-    return(
-      <div>
-        <button onScroll={this.handleScroll}>Scroll</button>
-      </div>
-    )
-  }
+      </section>
+    </div>
+  )
 }
+
+export default Header;
